eccf76f3214cb7a86e2f272512b642e36bc8ba82
ErikCruz <erikcruzdev@gmail.com>
2024-10-04 18:28:12 -0300

    Add Transform and Rotate custom functions

---
+++ Math/RotateTranslate/Assets/Scripts/Drive.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, translation, 0);
+        transform.position = HolisticMath.Translate(new Coords(transform.position), new Coords(transform.up), new Coords(0, translation, 0)).ToVector();
+
+        transform.up = HolisticMath.Rotate(new Coords(transform.up), rotation * Mathf.Deg2Rad, true).ToVector(); 
+        //We need to convert degrees to radians, since unity expects radians
+    }
+}
