b67a4b2409c62f9ffe2a805307be94bc8c5b3ecd
ErikCruz <erikcruzdev@gmail.com>
2024-10-16 16:12:11 -0300

    Sheep in the grass exercise

--- Math/UnityLinePlaneRay/Assets/Scripts/SheepMovementOnPlane.cs
+++ Math/UnityLinePlaneRay/Assets/Scripts/SheepMovementOnPlane.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaneIntersection : MonoBehaviour
+public class SheepMovementOnPlane : MonoBehaviour
 {
     //[SerializeField] Transform corner1;
     //[SerializeField] Transform corner2;
@@ -16,9 +16,9 @@ public class PlaneIntersection : MonoBehaviour
     void Start()
     {
         Vector3[] verticies = quad.GetComponent<MeshFilter>().mesh.vertices;
-        mPlane = new Plane(quad.transform.TransformPoint(verticies[0] ), 
-            quad.transform.TransformPoint(verticies[1] ),
-            quad.transform.TransformPoint(verticies[2] )
+        mPlane = new Plane(quad.transform.TransformPoint(verticies[0] + new Vector3(0, 2.9f, 0)), //sum to the sheep do not sink into the ground
+            quad.transform.TransformPoint(verticies[1] + new Vector3(0, 2.9f, 0)),
+            quad.transform.TransformPoint(verticies[2] + new Vector3(0, 2.9f, 0))
             );
         //mPlane = new Plane(corner1.position, corner2.position, corner3.position);
         Debug.Log(verticies[0]);
@@ -37,7 +37,10 @@ public class PlaneIntersection : MonoBehaviour
             if (mPlane.Raycast(ray, out t))
             {
                 Vector3 hitPoint = ray.GetPoint(t);
-              
+                float hitPointX = hitPoint.x;
+                float hitPointZ = hitPoint.z;
+                //keep sheep in the bounds of the plane
+                if(hitPointX >= -6 && hitPointX <= 6 && hitPointZ >= -6 && hitPointZ<= 6)
                     sphere.transform.position = hitPoint;
             }
         }
